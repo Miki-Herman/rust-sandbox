b3aff3887c27160e0cfa6ad37a3e336bc477f207
Miki-herman <her.mikiherman@gmail.com>
2025-10-11 19:03:00 +0200

    feat - finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,30 @@
+use std::io; 
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number from 1-100!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    loop {
+        println!("Please enter your guess:");
+       
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        // converts the string to 32-bit unsigned number
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },       
+        }
+    };
 }
