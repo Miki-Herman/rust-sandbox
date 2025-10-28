e5d24a7de5b13b28d3dd027d247162c0b9db3ada
Miki-herman <her.mikiherman@gmail.com>
2025-10-28 19:42:24 +0100

    feat - adding new funcs to structs :D

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,16 +7,30 @@ impl Rectangle {
     fn area(&self) -> u32 {
         return self.width * self.height;
     }
+
+    fn can_hold(&self, rect2: &Rectangle) -> bool {
+        return if self.area() >= rect2.area() {true} else {false}; 
+    }
 }
 
 fn main() {
-    let rectangle = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixles",
-        rectangle.area(),
-    );
+    let rect2 = Rectangle {
+        width: 15,
+        height: 20,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 70,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect3 hold rect1? {}", rect3.can_hold(&rect1));
+
 }
