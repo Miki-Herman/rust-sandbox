b3122ac1de2fd7c53682a02247483e32538e8f3e
Miki-herman <her.mikiherman@gmail.com>
2025-11-05 20:21:14 +0100

    feat - refactor ?

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,22 +1,7 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        pub fn seat_at_table() {}
-    }
-
-    pub mod serving {
-        pub fn take_order() {}
-
-        pub fn serve_order() {}
-
-        pub fn take_payment() {}
-    }
-}
-
-use front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
     hosting::add_to_waitlist();
 }
